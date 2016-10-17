a47a3f293eb1e14e9233be5a48eab6fbaa868e6e
Dennell Lucero <dennell.lucero@gmail.com>
2016-10-17 21:52:18 +0000

    8 tests working!

--- test/test-server.js
+++ test/test-server.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 
 var server = require('../server.js');
-var Item = require('../models/item');
+var Item = require('../models/items');
 
 var should = chai.should();
 var app = server.app;
@@ -27,21 +27,15 @@ describe('Shopping List', function() {
         chai.request(app)
             .get('/items')
             .end(function(err, res) {
-                //err {}  === null  - true
-                //err {message:broken due to donna,status:400} === null  - false
-                function equal(a,b){
-                    return (a === b)
-                }
-                console.log(equal(err,null),'should be true')
                 should.equal(err, null);
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('array');
                 res.body.should.have.length(3);
                 res.body[0].should.be.a('object');
-                res.body[0].should.have.property('id');
+                res.body[0].should.have.property('_id');
                 res.body[0].should.have.property('name');
-                res.body[0].id.should.be.a('number');
+                res.body[0]._id.should.be.a('string');
                 res.body[0].name.should.be.a('string');
                 res.body[0].name.should.equal('Broad beans');
                 res.body[1].name.should.equal('Tomatoes');
@@ -57,92 +51,190 @@ describe('Shopping List', function() {
                 should.equal(err, null);
                 res.should.have.status(201);
                 res.should.be.json;
-                console.log(res.body);
                 res.body.should.be.a('object');
                 res.body.should.have.property('name');
-                res.body.should.have.property('id');
+                res.body.should.have.property('_id');
                 res.body.name.should.be.a('string');
-                res.body.id.should.be.a('number');
+                res.body._id.should.be.a('string');
                 res.body.name.should.equal('Kale');
-                storage.items.should.be.a('array');
-                storage.items.should.have.length(4);
-                storage.items[3].should.be.a('object');
-                storage.items[3].should.have.property('id');
-                storage.items[3].should.have.property('name');
-                storage.items[3].id.should.be.a('number');
-                storage.items[3].name.should.be.a('string');
-                storage.items[3].name.should.equal('Kale');
+                chai.request(app)
+                    .get('/items')
+                    .end(function(err, res) {
+                        should.equal(err, null);
+                        res.body.length.should.equal(4);
+                        done();
+                    });
+            });
+    });
+    it('should not POST if name exists in storage', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({'name': 'Peppers'})
+            .end(function(err, res) {
+                err.should.not.be.null;
+                err.should.have.status(409);
+                res.should.be.json;
                 done();
             });
     });
-    
-    /*it('should not add blank on POST', function(done) {
+    it('should not POST if id exists', function(done) {
         chai.request(app)
             .post('/items')
-            .send({'name': null})
-            .end(function(err, res) {
-            should.equal(err, null);
-            res.should.have.status(400);
-        });
-    });     */  
-
-    it('should list items on PUT', function(done) {
-        chai.request(app)
-            .put('/items/1')
-            .send({'name': 'Kale', 'id': 1})
-            .end(function(err, res) {
-                res.should.have.status(200);
+            .send({'name': 'Peppers', '_id': 3})
+            .end(function (err, res) {
+                err.should.not.be.null;
+                res.should.have.status(409);
                 res.should.be.json;
-                res.should.be.an('object');
-                res.body.should.have.property('name');
-                res.body.should.have.property('id');
-                res.body.id.should.be.a('number');
-                res.body.name.should.be.a('string');
-                res.body.name.should.equal('Kale');
                 done();
-            })
+            });
     });
-    
-    it('should delete an item on delete', function(done) {
+    it('should not POST if body data does not exist', function(done) {
         chai.request(app)
-        .delete('/items/2')
+            .post('/items')
+            .send()
+            .end(function(err,res){
+                err.should.not.be.null;
+                //res.should.have.status(400);
+                res.should.be.json;
+                done();
+            });
+    });
+    it('should not POST without valid json', function(done) {
+        chai.request(app)
+        .post('/items')
+        //.send(variable = "variable")
         .end(function(err, res) {
-            res.should.have.status(200);
-            res.body.should.be.an('array');
-            res.body[0].should.be.an('object');
-            res.body[0].should.have.property('name');
-            res.body[0].should.have.property('id');
-            res.body[0].id.should.be.a('number');
-            res.body[0].name.should.be.a('string');
-            expect(res.body[3]).equal(undefined);
+            err.should.not.be.null;
+            //res.body.should.equal('item name missing');
+            //res.should.have.status(400);
+            res.should.be.json;
+            done();
+        });
+    });
+    it('should edit an item on PUT', function(done) {
+        chai.request(app)
+        .put('/items/3')
+        .send({'name': 'Pepper', '_id': 3})
+        .end(function(err, res) {
+            //should.equal(err, null);
+            //res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('name');
+            res.body.should.have.property('_id');
+            res.body.name.should.be.a('string');
+            res.body._id.should.be.a('number');
+            res.body.name.should.equal('Pepper');
             done();
         })
-        
     });
-
-    it('should be empty when all items deleted', function(done) {
-        chai.request(app).delete('/items/1').then(function() {
-            chai.request(app).delete('/items/2').then(function() {
-                chai.request(app).delete('/items/3').then(function() {
-                    chai.request(app).delete('/items/4')
-                    .end(function(err, res) {
-                        res.should.have.status(200);
-                        res.body.should.be.empty;
-                        done();
-                    })
-                })
-            })
+    it('should not PUT without id in endpoint', function(done) {
+        chai.request(app)
+        .put('/items')
+        .send({'name': 'Pepper', '_id': 3})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            res.should.have.status(404);
+            //res.body.should.equal('id param missing');
+            res.should.be.json;
+            done();
         })
-        
-   })
-
-
-    //it('should add an item on post');
-    //('should edit an item on put');
-    //it('should delete an item on delete');
-    //it('should be empty when all items deleted');
-    it('should not add blank on POST',function(done) {
-        done();
+    });
+    it('should not PUT with different endpoint and body id', function(done) {
+        chai.request(app)
+        .put('/items/2')
+        .send({'name': 'Pepper', '_id': 3})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            //res.should.have.status(400);
+            //res.body.should.equal('bad request');
+            res.should.be.json;
+            done();
+        })
+    });
+    it('PUT to id that doesn\'t exist', function(done) {
+        chai.request(app)
+        .put('/items/5')
+        .send({'name': 'Donut', '_id': 5})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('name');
+            res.body.should.have.property('_id');
+            res.body.name.should.be.a('string');
+            res.body._id.should.be.a('string');
+            res.body.name.should.equal('Donut');
+            done();
+        });
+    });
+    it('should not PUT without body data', function(done) {
+        chai.request(app)
+        .put('/items/2')
+        .send()
+        .end(function(err, res) {
+            err.should.not.be.null;
+            //res.should.have.status(400);
+            //res.body.should.equal('bad request');
+            res.should.be.json;
+            done();
+        });
+    });
+    it('should not put without valid json', function(done) {
+        chai.request(app)
+        .put('/items/2')
+        .send()//variable = "variable")
+        .end(function(err, res) {
+            err.should.not.be.null;
+            //res.should.have.status(400);
+            res.body.should.equal('bad request');
+            res.should.be.json;
+            done();
+        });
+    });
+    it('should DELETE an item on delete', function(done) {
+        chai.request(app)
+        .delete('/items/5')
+        .send({'name': 'Donut', '_id': 5})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.have.length(4);
+            JSON.stringify(storage.items).should.equal(JSON.stringify(res.body));
+            done();
+        });
+    });
+    it('should not DELETE id that doesn\'t exist', function(done) {
+        chai.request(app)
+        .delete('/items/6')
+        .send({'name': 'Cake', '_id': 6})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            //res.should.have.status(404);
+            res.should.be.json;
+            //res.body.should.equal('item not found');
+            chai.request(app)
+            .get('/items')
+            .end(function(err, res) {
+                should.equal(err, null)
+                res.body.should.equal(4);
+                done();
+            });
+        });
+    });
+    it('should not DELETE without id in endpoint', function(done) {
+        chai.request(app)
+        .delete('/items')
+        .send({'name': 'Pepper', '_id': 3})
+        .end(function(err, res) {
+            err.should.not.be.null;
+            res.should.have.status(404);
+            //res.body.should.equal('id param missing');
+            res.should.be.json;
+            done();
+        });
     });
 });
 
